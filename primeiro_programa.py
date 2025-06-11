e29d68819a5c76c02cc24977a8a40177fc211f1f
Markinnd <marcus.a.dornelas@gmail.com>
2025-06-11 00:05:46 -0300

    primeiro commit

---
+++ primeiro_programa.py
@@ -0,0 +1,9 @@
+#Formação - DIO - Python Fundamentals
+
+#primeiro programa em Python
+
+
+## Este programa imprime uma mensagem na tela
+
+print("Olá, Mundo!")  # Imprime a mensagem "Olá, Mundo!" na tela
+# Fim do programa
