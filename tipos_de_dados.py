4ca3d5a69b7bb909f237393efd8032ad744b1517
Markinnd <marcus.a.dornelas@gmail.com>
2025-06-11 00:24:33 -0300

    conhecendo a linguagem Python

---
+++ tipos_de_dados.py
@@ -0,0 +1,12 @@
+
+#tipos numericos
+print (1 + 1)  # Soma dois números e imprime o resultado
+print (0.5 + 0.5)  # Soma dois números decimais e imprime o resultado
+print (1 + 1.0)  # Soma um número inteiro e um decimal, imprimindo o resultado      
+
+#tipos booleanos
+print (True)  # Imprime o valor booleano True
+print (False)  # Imprime o valor booleano False
+
+#tipos de strings
+print ("Olá, Mundo!")  # Imprime a string "Olá, Mundo!"
